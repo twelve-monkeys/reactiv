b374d79738b3808a1c2f776e1a2006fc0cda6773
Jeremy Kothe <jeremy.robert.kothe@gmail.com>
2016-05-17 00:34:21 +1000

    tests run

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
     frameworks: ['jasmine', 'browserify'],
     // list of files / patterns to load in the browser
     files: [
-      'bin/spec.js'
+      'bin/tests/spec.js'
     ],
 
     // list of files to exclude
@@ -21,7 +21,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-       'bin/spec.js': [ 'browserify' ]
+       'bin/tests/spec.js': [ 'browserify' ]
     },
 
 
